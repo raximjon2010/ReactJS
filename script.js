71710014d60bb690eac6917061ca2a87ef094b83
raximjon2010 <rahimjonxujaqulov9@gmail.com>
2023-12-19 18:59:56 +0300

    1_dars

---
+++ script.js
@@ -0,0 +1,23 @@
+// =========================== Readme.md ===========================
+// Bu yerda Readme.md bu githubga kiradigan bolsak Proyektlarni tagida yoziladi huddi shu narsa 
+// reactda readme.md faylni ichida yoziladi. Buni o'chirib tashlasak ham boladi 
+// yoki uni malumotlarini ozgartirsak ham boladi
+// =========================== package.json ===========================
+// package.json => Bunda configirusionlari yoziladi.dependencies da qanaqa kutubxonalarda foydalanilgani va scripts degan joyida 
+// biz react ni qanaqa usulda brauzerda koradigan qilgan bolsak shular boladi va boshqa configusionlari boladi
+// =========================== gitgnore ===========================
+// Bunda esa qanaqa fayllar githubda korinmasligi yoziladi
+// =========================== node_modules ===========================
+// Buni deyarlini ichini ozgartirmiymiz
+// ==========================================================
+// Biz reactda project qilganimizda butun boshli projectda faqat bitta 
+// index.html fayli boladi- Shuning uchun react SinglePageApliciton deyiladi.
+// ==========================================================
+// import React from "react"; // Biz bu yerda react kutubxonasidan foydalanayotganini yozilgan
+// import  ReactDOM  from "react-dom"; // react dom kutubxonsidan yozilgan
+// let title = <h1>Hi react</h1> 
+// jsx - javascript exsemis
+// Html ichida javascript kodi yozishda {} shundan foydalanamiz
+// ReactDOM.render(title, document.getElementById('root') )
+
+
---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <h1>Hi React</h1>
+    <p>Description</p>
+  </React.StrictMode>
+);
+
+// import React from "react";
+// import  ReactDOM  from "react-dom";
+// let title = <h1>Hi react</h1>
+// jsx - javascript exsemis-
+// Html ichida javascript kodi yozishda {} shundan foydalanamiz
+// ReactDOM.render(title, document.getElementById('root') )
